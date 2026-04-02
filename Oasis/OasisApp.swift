16702c98d4b383e5a28b3bc732adccab7fc400a3
Edgardo <smokeshadowedgardo123@gmail.com>
2026-04-01 17:18:45 -0700

    Initial Commit

---
+++ Oasis/OasisApp.swift
@@ -0,0 +1,17 @@
+//
+//  OasisApp.swift
+//  Oasis
+//
+//  Created by Edgardo Ramos on 4/1/26.
+//
+
+import SwiftUI
+
+@main
+struct OasisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
